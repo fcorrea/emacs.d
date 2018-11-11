cfb500e45fcac26d428b71ce1919cf9c5381d77a
Fernando Correa Neto <fcdoth@gmail.com>
2018-11-11 21:14:00 -0200

    more like a backup

--- init.el
+++ init.el
@@ -14,13 +14,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (base16-default-dark)))
+ '(ansi-color-names-vector
+   ["#21252B" "#E06C75" "#98C379" "#E5C07B" "#61AFEF" "#C678DD" "#56B6C2" "#ABB2BF"])
+ '(ansi-term-color-vector
+   [unspecified "#151515" "#fb9fb1" "#acc267" "#ddb26f" "#6fc2ef" "#e1a3ee" "#6fc2ef" "#d0d0d0"] t)
+ '(custom-enabled-themes (quote (clues)))
  '(custom-safe-themes
    (quote
-    ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "4b2679eac1095b60c2065187d713c39fbba27039d75c9c928a1f3b5d824a3b18" "bcfc77fcc3e012941eb47d5037f0fac767e23fd2dae039214e5fa856ac8bdfdd" "65f35d1e0d0858947f854dc898bfd830e832189d5555e875705a939836b53054" "44961a9303c92926740fc4121829c32abca38ba3a91897a4eab2aa3b7634bed4" default)))
+    ("adf5275cc3264f0a938d97ded007c82913906fc6cd64458eaae6853f6be287ce" "2642a1b7f53b9bb34c7f1e032d2098c852811ec2881eec2dc8cc07be004e45a0" "f11e219c9d043cbd5f4b2e01713c2c24a948a98bed48828dc670bd64ae771aa1" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" "1025e775a6d93981454680ddef169b6c51cc14cea8cb02d1872f9d3ce7a1da66" "840db7f67ce92c39deb38f38fbc5a990b8f89b0f47b77b96d98e4bf400ee590a" "f5f3a6fb685fe5e1587bafd07db3bf25a0655f3ddc579ed9d331b6b19827ea46" "41eb3fe4c6b80c7ad156a8c52e9dd6093e8856c7bbf2b92cc3a4108ceb385087" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "196df8815910c1a3422b5f7c1f45a72edfa851f6a1d672b7b727d9551bb7c7ba" "ecfd522bd04e43c16e58bd8af7991bc9583b8e56286ea0959a428b3d7991bbd8" "0c3b1358ea01895e56d1c0193f72559449462e5952bded28c81a8e09b53f103f" "99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "87d46d0ad89557c616d04bef34afd191234992c4eb955ff3c60c6aa3afc2e5cc" "fede08d0f23fc0612a8354e0cf800c9ecae47ec8f32c5f29da841fe090dfc450" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "4b2679eac1095b60c2065187d713c39fbba27039d75c9c928a1f3b5d824a3b18" "bcfc77fcc3e012941eb47d5037f0fac767e23fd2dae039214e5fa856ac8bdfdd" "65f35d1e0d0858947f854dc898bfd830e832189d5555e875705a939836b53054" "44961a9303c92926740fc4121829c32abca38ba3a91897a4eab2aa3b7634bed4" default)))
+ '(fci-rule-color "#3E4451")
  '(package-selected-packages
    (quote
-    (cargo flycheck-rust flymake-rust rustic rust-mode parinfer litable go-autocomplete auto-complete exec-path-from-shell go-mode remember-last-theme base16-theme))))
+    (atom-dark-theme atom-one-dark-theme clues-theme sublime-themes go-dlv smooth-scrolling buster-mode magit cargo flycheck-rust flymake-rust rustic rust-mode parinfer litable go-autocomplete auto-complete exec-path-from-shell go-mode remember-last-theme base16-theme)))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,6 +44,12 @@
 ;; Toolbar and menu off
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+
+;; Less jerky moves
+(smooth-scrolling-mode 1)
+
+;; I need to see numbers
+(global-linum-mode t)
 
 ;; Makes *scratch* empty.
 (setq initial-scratch-message "")
